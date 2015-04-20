79a3c0aabdd40ad12628e0ff264c157f43003db3
manuel-richter <manu@cmpjmitechs-MacBook-Pro-2.local>
2015-04-20 10:40:48 -0700

    WatchSide
    - Colorchange
    - Wormhole communication between phone and watch

--- BlinkWatch/ViewController.swift
+++ BlinkWatch/ViewController.swift
@@ -12,40 +12,46 @@ import AVFoundation
 import GLKit
 import Accelerate
 
-var wormhole = MMWormhole(applicationGroupIdentifier: "group.bosch.cmpjmi", optionalDirectory: "theWatch")
+//var wormhole = MMWormhole(applicationGroupIdentifier: "group.bosch.cmpjmi", optionalDirectory: "theWatch")
 
 
 class ViewController: UIViewController, EZMicrophoneDelegate {
     
+    
+    // MARK: - UI Objects
     @IBOutlet weak var printButton: UIButton!
     @IBOutlet weak var audioPlot: EZAudioPlotGL!
     @IBOutlet weak var beat: UILabel!
     
     
+    // MARK: - Attributes
     var microphone: EZMicrophone!
-    
     var appendedBuffer = [Double]()
     var appendedFft = [Double]()
-    
     var fftCounter = 0
     var oldEnergy = 0 as Double
     var bufferCounter = 0
     var lastBeat = -10
     
+    // Tunnel for data exchange
+    var wormhole = MMWormhole(applicationGroupIdentifier: "group.bosch.cmpjmi", optionalDirectory: "theWatch")
+
+    
+    
+    // MARK: - official SWIFT
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
         
-        
+        // initialize UI iphone
         self.microphone = EZMicrophone(microphoneDelegate: self)
-        
         self.audioPlot.backgroundColor = UIColor.blackColor()
         self.audioPlot.color = UIColor.orangeColor()
         self.audioPlot.plotType = EZPlotType.Rolling
         self.audioPlot.shouldFill = false
         self.audioPlot.shouldMirror = false
         
+        // start recording via internal microphone
         self.microphone.startFetchingAudio()
         
     }
@@ -55,7 +61,7 @@ class ViewController: UIViewController, EZMicrophoneDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    
+    // MARK: - BeatDetection
     func microphone(microphone: EZMicrophone!, hasAudioReceived buffer: UnsafeMutablePointer<UnsafeMutablePointer<Float>>, withBufferSize bufferSize: UInt32, withNumberOfChannels numberOfChannels: UInt32) {
         dispatch_async(dispatch_get_main_queue(), {
             
@@ -69,10 +75,10 @@ class ViewController: UIViewController, EZMicrophoneDelegate {
                 self.bufferCounter++
                 
                 for( var i = 0; i<511; i++){
-
+                    
                     superbuffer.append(Double(buffer[0][i]))
                 }
-
+                
                 
                 //find max in self.fft
                 let fftArray = self.fft(superbuffer)
@@ -85,7 +91,7 @@ class ViewController: UIViewController, EZMicrophoneDelegate {
                 
                 
                 
-
+                
                 
                 //let fftMax = fftArray[9...20].reduce(fftArray[9], { max($0, $1) })
                 
@@ -104,17 +110,16 @@ class ViewController: UIViewController, EZMicrophoneDelegate {
                             self.beat.hidden = false
                             self.audioPlot.backgroundColor = UIColor.whiteColor()
                             
-                            wormhole.passMessageObject("test", identifier: "mess")
-
-                            
+                            //wormhole.passMessageObject("test", identifier: "mess")
+                            self.wormhole.passMessageObject("BOOM \(NSDate())", identifier: "mess")
                             
                         } else {
                             
                             self.beat.hidden = true
                             self.audioPlot.backgroundColor = UIColor.blackColor()
                             
-                            wormhole.passMessageObject("test", identifier: "mess")
-
+                            //wormhole.passMessageObject("test", identifier: "mess")
+                            self.wormhole.passMessageObject("BOOM \(NSDate())", identifier: "mess")
                             
                         }
                         self.lastBeat = beat_c
