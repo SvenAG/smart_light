4cf4952fe9a5147e463e62020af6c4e62557662b
Nicolas <bosch@Boschs-MacBook-Pro.local>
2015-04-21 14:48:58 -0700

    hier ist alles drin, was wir bis jetzt haben, keine 1000 Versionen mehr

--- BlinkWatch/ViewController.swift
+++ BlinkWatch/ViewController.swift
@@ -28,10 +28,12 @@ class ViewController: UIViewController, EZMicrophoneDelegate {
     var oldEnergy = 0 as Double
     var bufferCounter = 0
     var lastBeat = -10
+    var energyArray = [0.0]
+    
     
     // Tunnel for data exchange
     var wormhole = MMWormhole(applicationGroupIdentifier: "group.bosch.cmpjmi", optionalDirectory: "theWatch")
-
+    
     
     // MARK: - official SWIFT
     override func viewDidLoad() {
@@ -56,9 +58,15 @@ class ViewController: UIViewController, EZMicrophoneDelegate {
         // Dispose of any resources that can be recreated.
     }
     
+    var supersuperbuffer = [Double]()
+    var bigBufferCounter = 1
+    
     // MARK: - BeatDetection
     func microphone(microphone: EZMicrophone!, hasAudioReceived buffer: UnsafeMutablePointer<UnsafeMutablePointer<Float>>, withBufferSize bufferSize: UInt32, withNumberOfChannels numberOfChannels: UInt32) {
         dispatch_async(dispatch_get_main_queue(), {
+            
+            
+            
             
             self.audioPlot.updateBuffer(buffer[0], withBufferSize: bufferSize)
             if(abs(buffer[0][0]) > 0.01){
@@ -67,92 +75,127 @@ class ViewController: UIViewController, EZMicrophoneDelegate {
                 
                 self.bufferCounter++
                 
-                for( var i = 0; i<511; i++){
+                for( var i = 0; i<512; i++){
                     
                     superbuffer.append(Double(buffer[0][i]))
                 }
                 
-                //find max in self.fft
-                let fftArray = self.fft(superbuffer)
-                
-                var lowerbound = 1
-                var upperbound = 3
-                
-                var fftEnergy = fftArray[lowerbound...upperbound].reduce(0, +)
-
-                
-                //let fftMax = fftArray[9...20].reduce(fftArray[9], { max($0, $1) })
-                
-                
-                println((fftEnergy - self.oldEnergy))
-                
-                if fftEnergy - self.oldEnergy > 0.05 {
-                    var beat_c = self.bufferCounter
+                self.supersuperbuffer = self.supersuperbuffer + superbuffer
+                self.bigBufferCounter++
+                if self.bigBufferCounter == 3 {
                     
-                    if(beat_c - self.lastBeat)>25{
-                        //BEAT
-                        if self.beat.hidden == true {
-                            
-                            self.beat.hidden = false
-                            self.audioPlot.backgroundColor = UIColor.whiteColor()
-                            
-                            //wormhole.passMessageObject("test", identifier: "mess")
-                            self.wormhole.passMessageObject("BOOM \(NSDate())", identifier: "mess")
-                            
-                        } else {
-                            
-                            self.beat.hidden = true
-                            self.audioPlot.backgroundColor = UIColor.blackColor()
-                            
-                            //wormhole.passMessageObject("test", identifier: "mess")
-                            self.wormhole.passMessageObject("BOOM \(NSDate())", identifier: "mess")
-                            
-                        }
-                        self.lastBeat = beat_c
+                    
+                    let fftArray = self.fft(self.supersuperbuffer)
+                    
+                    
+                    
+                    self.supersuperbuffer = []
+                    self.bigBufferCounter = 1
+                    
+                    var lowerbound = 1
+                    var upperbound = 64
+                    var fftEnergy = fftArray[lowerbound...upperbound].reduce(0, +)
+                    
+                    self.energyArray.append(fftEnergy)
+                    
+                    if self.energyArray.count > 43 {
+                        
+                   self.energyArray.removeAtIndex(0)
                     }
+                    //Calculate average
+                    
+                    var averageEnergy = Double(self.energyArray.reduce(0, +)) / Double(self.energyArray.count)
+                    
+                    //Define Constant C for Energy Comparison
+                    var C = 1.0
+                    
+                    
+                    
+                    //Compare fftEnergy and averageEnergy -> Beat
+                    
+                    if fftEnergy > (C * averageEnergy) {
+                        var beat_c = self.bufferCounter
+                        
+                        if(beat_c - self.lastBeat)>10{
+                            //BEAT
+                            if self.beat.hidden == true {
+                                
+                                self.beat.hidden = false
+                                self.audioPlot.backgroundColor = UIColor.whiteColor()
+                                
+                                self.wormhole.passMessageObject("BOOM\(NSDate())", identifier: "mess")
+                                
+                                
+                                
+                            } else {
+                                
+                                self.beat.hidden = true
+                                self.audioPlot.backgroundColor = UIColor.blackColor()
+                                
+                                self.wormhole.passMessageObject("BOOM\(NSDate())", identifier: "mess")
+                                
+                                
+                            }
+                            self.lastBeat = beat_c
+                        }
+                        
+                    }
+                    
+                    
+                    
                 }
                 
-                //Save old Energy
                 
-                self.oldEnergy = fftEnergy
-                }
+                
+                
+                
+            }
+            
         })
-    }
-    
-    func microphone(microphone: EZMicrophone!, hasAudioStreamBasicDescription audioStreamBasicDescription: AudioStreamBasicDescription) {
-        EZAudio.printASBD(audioStreamBasicDescription)
-    }
-    
-    func microphone(microphone: EZMicrophone!, hasBufferList bufferList: UnsafeMutablePointer<AudioBufferList>, withBufferSize bufferSize: UInt32, withNumberOfChannels numberOfChannels: UInt32) {
-        //println(bufferList[0])
-    }
-    // MARK: - FFT
-    func fft(input: [Double]) -> [Double] {
-        var real = [Double](input)
-        var imaginary = [Double](count: input.count, repeatedValue: 0.0)
-        var splitComplex = DSPDoubleSplitComplex(realp: &real, imagp: &imaginary)
         
-        let length = vDSP_Length(floor(log2(Float(input.count))))
-        let radix = FFTRadix(kFFTRadix2)
-        let weights = vDSP_create_fftsetupD(length, radix)
-        vDSP_fft_zipD(weights, &splitComplex, 1, length, FFTDirection(FFT_FORWARD))
         
-        var magnitudes = [Double](count: input.count, repeatedValue: 0.0)
-        vDSP_zvmagsD(&splitComplex, 1, &magnitudes, 1, vDSP_Length(input.count))
         
-        var normalizedMagnitudes = [Double](count: input.count, repeatedValue: 0.0)
-        vDSP_vsmulD(sqrt(magnitudes), 1, [2.0 / Double(input.count)], &normalizedMagnitudes, 1, vDSP_Length(input.count))
-        
-        vDSP_destroy_fftsetupD(weights)
-        
-        return normalizedMagnitudes
     }
-    
-    func sqrt(x: [Double]) -> [Double] {
-        var results = [Double](count:x.count, repeatedValue:0.0)
-        vvsqrt(&results, x, [Int32(x.count)])
-        return results
-    }
-
+        
+        
+        
+        
+        
+        
+        func microphone(microphone: EZMicrophone!, hasAudioStreamBasicDescription audioStreamBasicDescription: AudioStreamBasicDescription) {
+            EZAudio.printASBD(audioStreamBasicDescription)
+        }
+        
+        func microphone(microphone: EZMicrophone!, hasBufferList bufferList: UnsafeMutablePointer<AudioBufferList>, withBufferSize bufferSize: UInt32, withNumberOfChannels numberOfChannels: UInt32) {
+            //println(bufferList[0])
+        }
+        // MARK: - FFT
+        func fft(input: [Double]) -> [Double] {
+            var real = [Double](input)
+            var imaginary = [Double](count: input.count, repeatedValue: 0.0)
+            var splitComplex = DSPDoubleSplitComplex(realp: &real, imagp: &imaginary)
+            
+            let length = vDSP_Length(floor(log2(Float(input.count))))
+            let radix = FFTRadix(kFFTRadix2)
+            let weights = vDSP_create_fftsetupD(length, radix)
+            vDSP_fft_zipD(weights, &splitComplex, 1, length, FFTDirection(FFT_FORWARD))
+            
+            var magnitudes = [Double](count: input.count, repeatedValue: 0.0)
+            vDSP_zvmagsD(&splitComplex, 1, &magnitudes, 1, vDSP_Length(input.count))
+            
+            var normalizedMagnitudes = [Double](count: input.count, repeatedValue: 0.0)
+            vDSP_vsmulD(sqrt(magnitudes), 1, [2.0 / Double(input.count)], &normalizedMagnitudes, 1, vDSP_Length(input.count))
+            
+            vDSP_destroy_fftsetupD(weights)
+            
+            return normalizedMagnitudes
+        }
+        
+        func sqrt(x: [Double]) -> [Double] {
+            var results = [Double](count:x.count, repeatedValue:0.0)
+            vvsqrt(&results, x, [Int32(x.count)])
+            return results
+        }
+        
 }
 
