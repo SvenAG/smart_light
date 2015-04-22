db596248cc4129fd8e1b66844e008964476f2703
manuel-richter <manu@cmpjmitechs-MacBook-Pro-2.local>
2015-04-22 11:34:52 -0700

    Play-Pause Functionality
    - Play/ Pause Button to start and stop the App on the watch
    - swap “receiving signals” to own function called getDataFromIphone

--- BlinkWatch WatchKit Extension/InterfaceController.swift
+++ BlinkWatch WatchKit Extension/InterfaceController.swift
@@ -12,46 +12,49 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
     
+    // Attributes
+    var oldOutput = NSString()
+    var oldRandomNumber = 1
+    var buttonIsActive = true
+    var showLightning = false
+    
+    var wormhole = MMWormhole(applicationGroupIdentifier: "group.bosch.cmpjmi", optionalDirectory: "theWatch")
+    
     
     @IBOutlet weak var buttonLabel: WKInterfaceButton!
     
-    var oldOutput = NSString()
-    var oldRandomNumber = 1
+    @IBAction func tappedButton() {
+        
+        // App is paused
+        if (buttonIsActive != false) {
+            
+            getDataFromIphone()
+            buttonLabel.setTitle("PLAY")
+            buttonIsActive = false
+        }
+        // App is running
+        else if (buttonIsActive != true) {
+            
+            buttonIsActive = true
+            buttonLabel.setTitle("PAUSE")
+        }
+        // shouldn't land here
+        else {
+            
+            println("no state here")
+        }
+    }
     
-    
-    var wormhole = MMWormhole(applicationGroupIdentifier: "group.bosch.cmpjmi", optionalDirectory: "theWatch")
     
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
-        // MARK: - Wormhole Listener (Receiver)
-        var listener: Void = wormhole.listenForMessageWithIdentifier("mess", listener: { (String) -> Void in
-            
-            // setting text of button label
-            var output : NSString = self.wormhole.messageWithIdentifier("mess") as NSString!
-            
-            // checking if new bass-sequence is different from last bass-sequence
-            if (output != self.oldOutput) {
-                
-                var randomNumber = Int(arc4random_uniform(5))
-                
-                if randomNumber == self.oldRandomNumber {
-                    if randomNumber == 0 {
-                        randomNumber += 1
-                    }
-                    else{
-                        randomNumber -= 1
-                    }
-                }
-                self.changeBackgroundColor(output, index: randomNumber)
-                self.oldRandomNumber = randomNumber
-                self.oldOutput = output
-                
-                // for debugging - will be deleted
-                println(output)
-                self.buttonLabel.setTitle(output)
-            }
-        })
+        // initialize variables
+        buttonIsActive = true
+        buttonLabel.setTitle("PAUSE")
+        
+        getDataFromIphone()
+        
     }
     
     override func willActivate() {
@@ -73,9 +76,47 @@ class InterfaceController: WKInterfaceController {
         farbPalette = [UIColor.blueColor(), UIColor.redColor(), UIColor.magentaColor(), UIColor.greenColor(), UIColor.yellowColor()]
         
         // for debugging - will be deleted
-        println("LUME with index: \(index)")
+        //println("LUME with index: \(index)")
         
-        buttonLabel.setBackgroundColor(farbPalette[index])
+        if(buttonIsActive != false) {
+          buttonLabel.setBackgroundColor(farbPalette[index])
+        }
+        else {
+            println("No color change")
+        }
+        
+    }
+    
+    // MARK: - Wormhole Listener (Receiver)
+    func getDataFromIphone() {
+        
+        var listener: Void = wormhole.listenForMessageWithIdentifier("mess", listener: { (String) -> Void in
+            
+            // setting text of button label
+            var output : NSString = self.wormhole.messageWithIdentifier("mess") as NSString!
+            
+            // checking if new bass-sequence is different from last bass-sequence
+            if (output != self.oldOutput) {
+                
+                var randomNumber = Int(arc4random_uniform(5))
+                
+                if randomNumber == self.oldRandomNumber {
+                    if randomNumber == 0 {
+                        randomNumber += 1
+                    }
+                    else {
+                        randomNumber -= 1
+                    }
+                }
+                self.changeBackgroundColor(output, index: randomNumber)
+                self.oldRandomNumber = randomNumber
+                self.oldOutput = output
+                
+                // for debugging - will be deleted
+                //println(output)
+                //self.buttonLabel.setTitle(output)
+            }
+        })
     }
     
 }
